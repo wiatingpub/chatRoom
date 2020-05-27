fda4d1f2b9c285001bc498fb8c049bfaa39c3c82
lixifan <1359704355@qq.com>
2020-05-27 08:30:50 -0700

    add, 提交源码

---
+++ chatRoom/src/main/java/com/nuofankj/chatRoom/ChatRoomApplication.java
@@ -0,0 +1,13 @@
+package com.nuofankj.chatRoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatRoomApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatRoomApplication.class, args);
+    }
+}
---
+++ chatRoom/src/test/java/com/nuofankj/chatRoom/ChatRoomApplicationTests.java
@@ -0,0 +1,12 @@
+package com.nuofankj.chatRoom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
