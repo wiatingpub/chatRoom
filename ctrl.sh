845614318091e537cffa2d749ef3cb3636e85104
xifanxiaxue <1359704355@qq.com>
2020-06-06 17:05:17 +0800

    chatRoom, change, 优化docker配置

--- ctrl.sh
+++ ctrl.sh
@@ -1,2 +1,26 @@
 #!/bin/sh
-nohup sudo docker-compose up &
+case "$1" in
+    start)
+        nohup sudo docker-compose up &
+    ;;
+    stop)
+        docker stop $Container
+    ;;
+    remove)
+        docker stop $Container
+        docker rm $Container
+    ;;
+    login)
+        docker exec -it $Container /bin/bash
+    ;;
+    info)
+        docker ps -a -f name=$Container
+    ;;
+    port)
+        docker port $Container
+    ;;
+    *)
+        echo "Usage: build|run|init[chmod,conf]|start|stop|remove|login|info|port"
+        exit 3
+    ;;
+esac
