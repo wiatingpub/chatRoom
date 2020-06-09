ef44ab6a53056af1f4568123c1c51d5ccde58e2c
lixifan <1359704355@qq.com>
2020-06-09 15:03:44 +0800

    chatRoom, change, 优化docker配置

---
+++ chatRoom/ctrl.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+case "$1" in
+    build)
+        cd ../lib/socket
+        mvn install
+        cd ../../chatRoom/
+    ;;
+    start)
+        nohup sudo docker-compose up &
+    ;;
+    stop)
+        docker sudo docker-compose down
+    ;;
+    login)
+        docker exec -it $Container /bin/bash
+    ;;
+    info)
+        docker ps -a -f name=$Container
+    ;;
+    port)
+        docker port $Container
+    ;;
+    *)
+        echo "Usage: build|run|init[chmod,conf]|start|stop|remove|login|info|port"
+        exit 3
+    ;;
+esac
