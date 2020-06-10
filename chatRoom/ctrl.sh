23b9248196122e9b6818f0bad2976e52d7db59bd
lixifan <1359704355@qq.com>
2020-06-10 11:04:48 +0800

    chatRoom, change, 优化ctrl

--- chatRoom/ctrl.sh
+++ chatRoom/ctrl.sh
@@ -1,29 +1,31 @@
 #!/bin/sh
 case "$1" in
-    build)
-        cd ../lib/socket
-        mvn install
-        cd ../../chatRoom/
-        cp ./conf.d/chatRoom.conf /home/ubuntu/proj/nginx-center/conf.d/chatRoom.conf
-    ;;
-    start)
-        nohup sudo docker-compose up > chatRoom.log 2>&1 &
-        tail -f chatRoom.log
-    ;;
-    stop)
-        sudo docker-compose down
-    ;;
-    login)
-        docker exec -it $Container /bin/bash
-    ;;
-    info)
-        docker ps -a -f name=$Container
-    ;;
-    port)
-        docker port $Container
-    ;;
-    *)
-        echo "Usage: build|run|init[chmod,conf]|start|stop|remove|login|info|port"
-        exit 3
-    ;;
+build)
+  cd ../lib/socket
+  sudo rm -rf target
+  mvn install
+  cd ../../chatRoom/
+  sudo rm -rf target
+  cp ./conf.d/chatRoom.conf /home/ubuntu/proj/nginx-center/conf.d/chatRoom.conf
+  ;;
+start)
+  nohup sudo docker-compose up >chatRoom.log 2>&1 &
+  tail -f chatRoom.log
+  ;;
+stop)
+  sudo docker-compose down
+  ;;
+login)
+  docker exec -it $Container /bin/bash
+  ;;
+info)
+  docker ps -a -f name=$Container
+  ;;
+port)
+  docker port $Container
+  ;;
+*)
+  echo "Usage: build|run|init[chmod,conf]|start|stop|remove|login|info|port"
+  exit 3
+  ;;
 esac
